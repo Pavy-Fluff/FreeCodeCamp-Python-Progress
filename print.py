19b66a0cd3a8e08989c1e2b5a3767a0a86623d62
Pavy <welcome.pavy@gmail.com>
2026-02-22 21:05:16 +0200

    Print functions.

--- print.py
+++ print.py
@@ -1,2 +1,5 @@
+# FreeCodeCamp - Python Basics
+# Lesson: Print functions
+
 print('Hello world!') # Hello world!
 print('My favorite colors are', 'black,', 'white.') # My favorite colors are black, white.
