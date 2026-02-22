8b7e258f3b46fa2ee02c2eb90bf67a89ea2bac91
Pavy <welcome.pavy@gmail.com>
2026-02-22 21:04:31 +0200

    Clarified concepts

--- variables.py
+++ variables.py
@@ -1,3 +1,6 @@
+# FreeCodeCamp - Python Basics
+# Lesson: Variables
+
 name = "Pavy" # My name. This is a string.
 alphabet = "abcdefghijklmnopqrstuvwxyz" # The alphabet! This is ALSO a string!
 age = 16 # my age when i've typed this. Also this is an integer.
