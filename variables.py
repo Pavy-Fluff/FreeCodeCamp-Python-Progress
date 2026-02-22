fc00b4093cfd0bc8ef5d25a83c92184f0835fc24
Pavy <welcome.pavy@gmail.com>
2026-02-22 17:52:45 +0200

    integers/strings

--- variables.py
+++ variables.py
@@ -1,2 +1,3 @@
-name = "Pavy"
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+name = "Pavy" # My name. This is a string.
+alphabet = "abcdefghijklmnopqrstuvwxyz" # The alphabet! This is ALSO a string!
+age = 16 # my age when i've typed this. Also this is an integer.
