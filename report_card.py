9ab48a7987c59fee73ec919bb85a33a5b6035ff8
Pavy <welcome.pavy@gmail.com>
2026-02-23 08:23:27 +0200

    my own report card

---
+++ report_card.py
@@ -0,0 +1,14 @@
+# FreeCodeCamp - Python Basics
+# Building my own report card
+
+name = 'Pavy'
+print(name, type(name)) # Pavy <class 'str'> 
+
+age = 16
+print(age, type(age)) # <class 'int'>
+
+score = 87.2
+print(isinstance(score, float)) # True
+
+student_is_learning = True
+print(type(student_is_learning)) # True
