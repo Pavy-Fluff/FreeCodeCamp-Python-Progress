e539f71bc76b28be3d6e93c815169b37ee9dd046
Pavy <welcome.pavy@gmail.com>
2026-02-24 16:19:18 +0200

    Built the version2.0 for report_card.py

---
+++ profile.py
@@ -0,0 +1,20 @@
+# FreeCodeCamp - Python Basics
+# my "better" report_card.py
+
+name = "Pavy"
+age = 16
+position = "Home"
+salary = 0
+learning = "IT"
+experience = 1 + ' year'
+hobby = "tapping"
+
+# Ugly way to present yourself
+
+introduction = name + ' ' + str(age) + ' ' + position + ' ' + str(salary) + ' ' + learning + ' ' + str(experience) + ' ' + hobby 
+print(introduction)
+
+# Better way
+
+introduction = f'Name: {name} | Age: {age} | Position: {position} | Salary: {salary} | Learns: {learning} | Experience: {experience} | Hobby: {hobby}'
+print(introduction)
